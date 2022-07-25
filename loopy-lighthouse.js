8cc8273fc2b046014447757797c52971776da398
Nicholas <nc_chau@hotmail.com>
2022-07-25 23:17:38 +0000

    example of function using hello+name

---
+++ loopy-lighthouse.js
@@ -0,0 +1,11 @@
+for (let num = 100; num <= 200; num++) {
+  if (num % 3 === 0 && num % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (num % 3 === 0) {
+    console.log("Loopy");
+  } else if (num % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(num);
+  }
+}
