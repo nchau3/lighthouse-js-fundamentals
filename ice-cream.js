f1f144d4dfbe230f099529d7042e743cf76b65e0
Nicholas <nc_chau@hotmail.com>
2022-07-29 00:56:03 +0000

    exercise for accessing arrays

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// add "root beer"
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+//log first flavour
+console.log(iceCreamFlavours[0]);
+//log last flavour
+console.log(iceCreamFlavours[5]);
+//log total number of flavours
+console.log(iceCreamFlavours.length);
