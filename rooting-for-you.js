4c9eb044bad2f8c8139850c21e2fe4e77cc11674
Nicholas <nc_chau@hotmail.com>
2022-08-02 00:15:56 +0000

    finding highest values in array, tournament style

---
+++ rooting-for-you.js
@@ -0,0 +1,35 @@
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 13,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 12,
+    plumpness: 3
+  },
+  {
+    submitter: 'Farmer Man',
+    redness: 14,
+    plumpness: 6
+  }
+]
+
+const metric = 'redness'
+
+const judgeVegetable = function (vegetables, metric) {
+  let j = 0;
+  for (let i = 0; i < vegetables.length; i++) {
+    if (vegetables[j][metric] < vegetables[i][metric]) {
+      j = i;
+    } 
+  } return vegetables[j].submitter;
+}
+
+console.log(judgeVegetable(vegetables, metric));
